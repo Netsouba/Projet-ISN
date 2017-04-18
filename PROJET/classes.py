799aaef7fa6aff4f0f72a5841dd1c23c0ee82bd9
Netsouba <netsouba@gmail.com>
2017-04-18 17:22:14 +0200

    Add files via upload

---
+++ PROJET/classes.py
@@ -0,0 +1,473 @@
+﻿import pygame
+from pygame.locals import *
+import math
+from copy import deepcopy
+from constantes import *
+from fonctions import *
+
+
+#----------------------------Classes---------------------------------------
+class Niveau():
+    liste=[]
+    """
+        Attributs:  theme :     Numero du theme
+                    fichier:    Nom du fichier
+                    structure:  Liste de liste
+                    dict_image: Images
+
+
+        Méthodes:   creation()
+                    update()
+
+    """
+
+    def __init__(self,n,c_fichier,images,theme):
+
+        self.numero=n
+        self.theme=theme    #Numero du theme
+        self.fichier=c_fichier
+
+        self.structure=[]
+        self.dict_element={ "bloc":[],
+                            "fin":None,
+                            "tp":[],
+                            "torche":[],
+                            "porte":[],
+                            "goomba":[],
+                            "bulle":[],
+                            "boule feu":[]
+                            }
+
+        self.liste_ombre=[]
+        self.vent=False,None
+        self.noir=False
+
+        self.dict_images=images
+        self.creation()
+        Niveau.liste.append(self)
+
+    def creation(self):
+
+        with open(self.fichier,'r') as fichier:
+            grille=fichier.readlines()
+            for ligne in grille:
+                if ligne[-1]=='\n':
+                    self.structure.append(ligne[:-1])
+                else:
+                    self.structure.append(ligne)
+
+
+        for y,ligne in enumerate(self.structure):
+            for x,car in enumerate(ligne):
+
+                if car=='1':
+                    r=Bloc(self.dict_images["bloc"],(x*bloc_x , y*bloc_y))
+                    self.dict_element["bloc"].append(r)
+
+                elif car=='2':
+                    f=Fin(self.dict_images["porte"],(x*bloc_x , y*bloc_y))
+                    self.dict_element["fin"]=f
+
+                elif car=='3':
+                    t=Teleport(self.dict_images["tp"],(x*bloc_x , y*bloc_y))
+                    self.dict_element["tp"].append(t)
+
+                elif car=='4':
+                    t=Torche(self.dict_images["torche"],(x*bloc_x , y*bloc_y))
+                    self.dict_element["torche"].append(t)
+
+                elif car=='5':
+                    p=Porte(self.dict_images["portail"],(x*bloc_x , y*bloc_y))
+                    self.dict_element["porte"].append(p)
+
+                elif car=='6':
+                    go=Goomba(self.dict_images["goomba"],(x*bloc_x , y*bloc_y))
+                    self.dict_element["goomba"].append(go)
+
+                elif car=="n":
+                    self.noir=True
+
+    def update(self,perso):
+        #Ombre
+        self.liste_ombre=[]
+        if self.noir==True:
+            for y in range(0,fenetre_y,self.dict_images["ombre"].get_height()):
+                for x in range(0,fenetre_x,self.dict_images["ombre"].get_width()):
+                    self.liste_ombre.append((x,y))
+
+
+        #Vent
+        liste_obj_vent=[]
+        for i in self.dict_element["goomba"]:
+            liste_obj_vent.append(i)
+        liste_obj_vent.append(perso)
+
+        if self.vent[0]==True:
+
+            for i in liste_obj_vent:
+
+                if self.vent[1]=="droite":
+                    i.acceleration_x=15
+                elif self.vent[1]=="gauche":
+                    i.acceleration_x=-15
+        else:
+            for i in liste_obj_vent:
+                i.acceleration_x=0
+
+class Personnage():
+
+    def __init__(self,pos,dict_img):
+
+        self.dict_img=dict_img
+        self.img=dict_img["droite"]["debout"]
+        self.rect=self.img.get_rect()
+        self.ancien=self.rect
+        self.rect.x=pos[0]
+        self.rect.y=pos[1]
+        self.vitesse_x=0
+        self.vitesse_y=0
+        self.acceleration_x=0
+        self.acceleration_y=g
+        self.animation=0
+        self.saut=True
+        self.double_saut=False
+        self.deplacement=False
+        self.direction="droite"
+
+
+    def update(self,d_frame,niveau_actuel):
+
+        liste_rect=[]
+
+        for i in niveau_actuel.dict_element["bloc"]:
+            liste_rect.append(i.rect)
+
+        for i in niveau_actuel.dict_element["porte"]:
+            if i.ouvert==False:
+                liste_rect.append(i.rect)
+
+        #Mouvement
+
+        self.ancien=self.rect
+
+        self.vitesse_x+=self.acceleration_x
+        self.vitesse_y+=self.acceleration_y
+
+        self.rect=self.rect.move(self.vitesse_x*d_frame,self.vitesse_y)
+
+        self.saut=True #Le perso est en saut dans tous les cas sauf cas il touche le sol
+
+
+        #Restraindre position dans la fenetre
+        if self.rect.bottom>=fenetre_y:
+            print("T'es mort!!!!!!!!!!!!!!!!!!!")
+        elif self.rect.top<0:
+            self.rect.top=0
+            self.vitesse_y=0
+        if self.rect.right>=fenetre_x:
+            self.rect.right=fenetre_x
+            self.vitesse_x=0
+        elif self.rect.left<=0:
+            self.rect.left=0
+            self.vitesse_x=0
+
+        #Collision
+        i_collision=self.rect.collidelistall(liste_rect)
+        for rect in [liste_rect[i] for i in i_collision]:
+            if self.rect.bottom>=rect.top and self.ancien.bottom<=rect.top:
+                self.rect.bottom=rect.top
+                self.vitesse_y=0
+            elif self.rect.top<=rect.bottom and self.ancien.top>=rect.bottom:
+                self.rect.top=rect.bottom
+                self.vitesse_y=0
+
+            elif self.rect.right>=rect.left and self.ancien.right<=rect.left:
+                self.rect.right=rect.left
+            elif self.rect.left<=rect.right and self.ancien.left>=rect.right:
+                self.rect.left=rect.right
+
+        for rect in liste_rect:
+            if (self.rect.bottom==rect.top) and ((self.rect.left>rect.left and self.rect.left<rect.right) or (self.rect.right>rect.left and self.rect.right<rect.right)):
+                self.saut=False
+                self.double_saut=False
+
+
+        #Animation
+
+        if self.deplacement==True:
+            self.img=self.dict_img[self.direction]["cours"][self.animation]
+        if self.saut==True:
+            self.img=self.dict_img[self.direction]["saute"]
+        if self.deplacement==False and self.saut==False:
+            self.img=self.dict_img[self.direction]["debout"]
+
+        #Verification victoire
+        if self.rect.collidelist(niveau_actuel.dict_element["goomba"])!=-1:
+            print("LOLOLOL! T'es mort!")
+        if self.rect.colliderect(niveau_actuel.dict_element["fin"].rect):
+            return "win"
+
+class Bloc():
+    def __init__(self,img,pos):
+        self.img=img
+        self.rect=self.img.get_rect()
+        self.rect.topleft=pos
+
+
+class Fin():
+    def __init__(self,img,pos):
+        self.img=img
+        self.rect=self.img.get_rect()
+        self.rect.topleft=pos
+
+class Goomba():
+    def __init__(self,liste_img,pos):
+        self.liste_img=liste_img
+        self.img=self.liste_img[0]
+        self.rect=self.img.get_rect()
+        self.rect.topleft=pos
+        self.ancien=self.rect
+        self.vitesse_x=-50
+        self.vitesse_y=0
+        self.acceleration_x=0
+        self.acceleration_y=g
+        self.animation=0
+
+    def update(self,d_frame,niveau_actuel):
+
+        liste_rect=[]
+
+        for i in niveau_actuel.dict_element["bloc"]:
+            liste_rect.append(i.rect)
+
+        for i in niveau_actuel.dict_element["porte"]:
+            if i.ouvert==False:
+                liste_rect.append(i.rect)
+
+
+        self.ancien=self.rect
+        self.vitesse_x+=self.acceleration_x
+        self.vitesse_y+=self.acceleration_y
+
+        self.rect=self.rect.move(self.vitesse_x*d_frame,self.vitesse_y)
+
+
+
+        #Restraindre position dans la fenetre
+        if self.rect.top>fenetre_y:
+            niveau_actuel.dict_element["goomba"].remove(self)
+            del self
+            return 0
+
+        if self.rect.top<0:
+            self.rect.top=0
+            self.vitesse_y=0
+        if self.rect.right>=fenetre_x:
+            self.rect.right=fenetre_x
+            self.vitesse_x=-50
+        elif self.rect.left<=0:
+            self.rect.left=0
+            self.vitesse_x=50
+
+        #Collision
+        i_collision=self.rect.collidelistall(liste_rect)
+        for rect in [liste_rect[i] for i in i_collision]:
+
+            if self.rect.bottom>=rect.top and self.ancien.bottom<=rect.top:
+                self.rect.bottom=rect.top
+                self.vitesse_y=0
+            elif self.rect.top<=rect.bottom and self.ancien.top>=rect.bottom:
+                self.rect.top=rect.bottom
+                self.vitesse_y=0
+
+            elif self.rect.right>=rect.left and self.ancien.right<=rect.left:
+                self.rect.right=rect.left
+                self.vitesse_x=-50
+            elif self.rect.left<=rect.right and self.ancien.left>=rect.right:
+                self.rect.left=rect.right
+                self.vitesse_x=50
+
+        #Animation
+        self.img=self.liste_img[self.animation]
+
+
+
+class Teleport():
+
+    def __init__(self,liste_img,pos):
+        self.etat=4
+        self.liste_img=liste_img
+        self.img=self.liste_img[self.etat]
+        self.rect=self.img.get_rect()
+        self.rect.topleft=pos
+
+    def update(self,perso):
+        d=distance(perso.rect.center,self.rect.center)
+
+        if d<100:       self.etat=0
+        elif d<200:     self.etat=1
+        elif d<300:     self.etat=2
+        elif d<400:     self.etat=3
+        else:           self.etat=4
+
+        self.img=self.liste_img[self.etat]
+
+class BouleFeu():
+
+    def __init__(self,dict_img,direction,perso):
+
+        self.dict_img=dict_img
+        self.direction=direction
+        self.img=self.dict_img[self.direction][0]
+
+        self.rect=self.img.get_rect()
+        self.rect.center=perso.rect.center
+        self.acceleration_x=0
+        self.acceleration_y=0
+
+        if self.direction=="droite":
+            self.vitesse_x=200
+            self.vitesse_y=0
+        elif self.direction=="gauche":
+            self.vitesse_x=-200
+            self.vitesse_y=0
+        elif self.direction=="haut":
+            self.vitesse_x=0
+            self.vitesse_y=-6
+        elif self.direction=="bas":
+            self.vitesse_x=0
+            self.vitesse_y=6
+
+
+        self.animation=0
+
+    def update(self,d_frame,niveau_actuel):
+
+
+        #Mouvement
+
+        self.vitesse_x+=self.acceleration_x
+        self.vitesse_y+=self.acceleration_y
+        self.rect=self.rect.move(self.vitesse_x*d_frame,self.vitesse_y)
+
+
+        #Animation
+        self.img=self.dict_img[self.direction][self.animation]
+
+        #Restraindre position dans la fenetre
+        if self.rect.left>fenetre_x or self.rect.bottom<0 or self.rect.right<0 or self.rect.top>fenetre_y:
+            niveau_actuel.dict_element["boule feu"].remove(self)
+            del self
+            return 0
+
+        #Collision
+
+        liste_collision=[]  #Liste des objets ou la boule s'arrete sans effets
+        for i in niveau_actuel.dict_element["bloc"]:
+            liste_collision.append(i.rect)
+        for i in niveau_actuel.dict_element["porte"]:
+            if i.ouvert==False:
+                liste_collision.append(i.rect)
+
+        i_collision_torche=self.rect.collidelist(niveau_actuel.dict_element["torche"])
+        i_collision_goomba=self.rect.collidelist(niveau_actuel.dict_element["goomba"])
+
+        if i_collision_torche!=-1 and niveau_actuel.dict_element["torche"][i_collision_torche].enflamme==False:
+            niveau_actuel.dict_element["torche"][i_collision_torche].enflamme=True
+            niveau_actuel.dict_element["boule feu"].remove(self)
+            del self
+            return 0
+
+        elif self.rect.collidelist(liste_collision)!=-1:
+            niveau_actuel.dict_element["boule feu"].remove(self)
+            del self
+            return 0
+
+        elif i_collision_goomba!=-1:
+            niveau_actuel.dict_element["goomba"].pop(i_collision_goomba)
+            niveau_actuel.dict_element["boule feu"].remove(self)
+            del self
+            return 0
+
+
+        #Eclairage
+        l=[]
+        for o in niveau_actuel.liste_ombre:
+            if distance(self.rect.topleft,o)<=100:
+                l.append(o)
+        for o in l:
+            niveau_actuel.liste_ombre.remove(o)
+
+class Torche():
+
+    def __init__(self,dict_img,pos):
+
+        self.dict_img=dict_img
+        self.img=dict_img["Arret"]
+        self.rect=self.img.get_rect()
+        self.rect.topleft=pos
+        self.enflamme=False
+        self.animation=0
+
+    def update(self,niveau_actuel):
+        if self.enflamme==True:
+            self.img=self.dict_img["Flamme"][self.animation]
+            #Eclairage
+            l=[]
+            for o in niveau_actuel.liste_ombre:
+                if distance(self.rect.topleft,o)<=100:
+                    l.append(o)
+            for i in l:
+                niveau_actuel.liste_ombre.remove(i)
+        else:
+            self.img=self.dict_img["Arret"]
+
+
+class Porte():
+
+    def __init__(self,liste_img,pos):
+        self.liste_img=liste_img
+        self.img=liste_img[0]
+        self.rect=self.img.get_rect()
+        self.rect.topleft=pos
+        self.ouvert=False
+        self.animation=0
+
+    def update(self):
+        self.img=self.liste_img[self.animation]
+
+
+class Bulle():
+    liste=[]
+    def __init__(self,img,obj):
+        self.img=img
+        self.obj=obj
+        self.rect=self.img.get_rect()
+        self.rect.center=self.obj.rect.center
+
+    def update(self,niveau_actuel):
+
+        self.rect.center=self.obj.rect.center
+        self.obj.acceleration_y=-g/2
+        t=type(self.obj)
+        if t!=Personnage:
+            if t==Goomba:
+
+                if self.obj not in niveau_actuel.dict_element["goomba"]:
+                    niveau_actuel.dict_element["bulle"].remove(self)
+                    del self
+                    return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PROJET/fonctions.py
@@ -0,0 +1,302 @@
+﻿import pygame
+from pygame.locals import *
+import math
+from copy import deepcopy
+from constantes import *
+
+#---------------------------Fonctions----------------------------------------
+
+#Fontions maths
+def moyenne(liste):
+    try:
+        return sum(liste)/len(liste)
+    except:
+        return 0
+def milieu(point_a,point_b):
+    ax,ay=point_a
+    bx,by=point_b
+    m=(ax+bx)/2,(ay+by)/2
+    return m
+
+def distance(point_a,point_b):
+    return math.sqrt((point_b[0]-point_a[0])**2+(point_b[1]-point_a[1])**2)
+
+def droite(point_a,point_b):
+    if point_a[0]>point_b[0]:
+        point_a,point_b=point_b,point_a
+    ax,ay=point_a
+    bx,by=point_b
+
+    try:
+        delta=(by-ay)/(bx-ax)
+        liste_point=[(x,delta*(x-ax)+ay) for x in range(ax,bx)]
+
+    except ZeroDivisionError:
+
+        liste_point=[(ax,y) for y in range(ay,by,3)]
+
+    return liste_point
+
+def croissance(liste):
+    """ Retourne les variations d'une liste (Minima+Maxima, Indices)"""
+
+    liste_c=[]
+    liste_p=[]
+    croissant=None
+
+    for i in range(len(liste)-1):
+        if liste[i+1]>liste[i]:
+
+            if croissant==None:
+                liste_c.append(True)
+
+            elif croissant==False:
+                liste_p.append(i)
+
+                liste_c.append(True)
+
+            croissant=True
+
+        elif liste[i+1]<liste[i]:
+
+            if croissant==None:
+                liste_c.append(False)
+            elif croissant==True:
+                liste_p.append(i)
+
+                liste_c.append(False)
+
+            croissant=False
+
+    return liste_p,liste_c
+
+def calc_ecart_type(liste):
+    moyenne_l=moyenne(liste)
+    l=[(i-moyenne_l)*(i-moyenne_l) for i in liste]
+    return math.sqrt(sum(l))
+
+def interdiagonalequadri(a,b,c,d):
+    v_ac=(c[0]-a[0],c[1]-a[1])
+    v_bd=(d[0]-b[0],d[1]-b[1])
+
+##    eq_a={a[0]+v_ac[0]*t,
+##          a[1]+v_ac[1]*t}
+##    eq_b={b[0]+v_bd[0]*t,
+##          b[1]+v_bd[1]*t}
+##
+##    a[0]+v_ac[0]*t=b[0]+v_bd[0]*k,
+##    a[1]+v_ac[1]*t=b[1]+v_bd[1]*k
+##
+##    t=(b[0]+v_bd[0]*k-a[0])/v_ac[0],
+##    a[1]+v_ac[1]*(b[0]+v_bd[0]*k-a[0])/v_ac[0]=b[1]+v_bd[1]*k
+##
+##    t=(b[0]+v_bd[0]*k-a[0])/v_ac[0],
+##    a[1]+(v_ac[1]*b[0]-v_ac[1]*a[0])/v_ac[0]-b[1]=v_bd[1]*k-v_ac[1]*v_bd[0]*k/v_ac[0]
+##
+##    t=(b[0]+v_bd[0]*k-a[0])/v_ac[0],
+##    k=(a[1]+(v_ac[1]*b[0]-v_ac[1]*a[0])/v_ac[0]-b[1])/(v_bd[1]-v_ac[1]*v_bd[0]/v_ac[0])
+
+    try:
+        x=b[0]+v_bd[0]*(a[1]+(v_ac[1]*b[0]-v_ac[1]*a[0])/v_ac[0]-b[1])/(v_bd[1]-v_ac[1]*v_bd[0]/v_ac[0])
+        y=b[1]+v_bd[1]*(a[1]+(v_ac[1]*b[0]-v_ac[1]*a[0])/v_ac[0]-b[1])/(v_bd[1]-v_ac[1]*v_bd[0]/v_ac[0])
+        return int(x),int(y)
+    except ZeroDivisionError:
+        return 'Erreur'
+
+
+#Fonctions reconnaissance
+def r_droite(liste_pos):
+    acroissement=[]
+    for i in range(len(liste_pos)-1):
+        try:
+            acroissement.append((liste_pos[i+1][1]-liste_pos[i][1])/(liste_pos[i+1][0]-liste_pos[i][0]))
+        except ZeroDivisionError:
+            pass
+
+
+    ecart_type=calc_ecart_type(acroissement)
+
+
+    if ecart_type<=1.5:
+        return True,droite(liste_pos[0],liste_pos[-1])
+    else:
+        return False,None
+
+def r_point(liste_pos):
+    liste_distance=[distance(liste_pos[0],point) for point in liste_pos]
+    moy= moyenne(liste_distance)
+    if moy<=1:
+        return True,liste_pos[0]
+    else:
+        return False,None
+
+
+def r_angle(liste_pos):
+
+    liste_x=[i[0] for i in liste_pos]
+    liste_y=[i[1] for i in liste_pos]
+
+    if liste_y==sorted(liste_y) or liste_y==reversed(sorted(liste_y)):
+
+        liste_pics,l_croissance=croissance(liste_x)
+        nb_pics=len(liste_pics)
+
+        if nb_pics==1:
+            if r_droite(liste_pos[:liste_pics[0]])[0] and r_droite(liste_pos[liste_pics[0]:])[0]:
+                if l_croissance==[True,False]:
+                    return "droite",liste_pos[liste_pics[0]]
+                elif l_croissance==[False,True]:
+                    return "gauche",liste_pos[liste_pics[0]]
+
+    if liste_x==sorted(liste_x) or liste_x==reversed(sorted(liste_x)):
+
+        liste_pics,l_croissance=croissance(liste_y)
+        nb_pics=len(liste_pics)
+
+        if nb_pics==1:
+            if r_droite(liste_pos[:liste_pics[0]])[0] and r_droite(liste_pos[liste_pics[0]:])[0]:
+                if l_croissance==[True,False]:
+                    return "bas",liste_pos[liste_pics[0]]
+                elif l_croissance==[False,True]:
+                    return "haut",liste_pos[liste_pics[0]]
+
+
+    return False,None
+
+
+
+def r_eclair(liste_pos):
+
+    liste_x=[i[0] for i in liste_pos]
+    liste_y=[i[1] for i in liste_pos]
+
+    liste_pics_x,l_croissance_x= croissance(liste_x)
+    nb_pics_x= len(liste_pics_x)
+
+    liste_pics_y,l_croissance_y= croissance(liste_y)
+    nb_pics_y= len(liste_pics_y)
+
+
+
+    if nb_pics_x==2:
+
+        if r_droite(liste_pos[:liste_pics_x[0]])[0] and r_droite(liste_pos[liste_pics_x[0]:liste_pics_x[1]])[0] and r_droite(liste_pos[liste_pics_x[1]:])[0]:
+            if l_croissance_x==[False,True,False]:
+                if (nb_pics_y==0 and l_croissance_y==[True]) or (nb_pics_y==2 and l_croissance_y==[True, False,True]):
+                    return True
+
+    return False
+
+def r_cercle(liste_pos):
+    liste_x=[i[0] for i in liste_pos]
+    liste_y=[i[1] for i in liste_pos]
+
+    for point in liste_pos:
+            if point[0]==max(liste_x):
+                a=point
+            if point[1]==min(liste_y):
+                b=point
+            if point[0]==min(liste_x):
+                c=point
+            if point[1]==max(liste_y):
+                d=point
+    centre=interdiagonalequadri(a,b,c,d)
+
+    if centre!="Erreur":
+        liste_distance=[distance(point,centre) for point in liste_pos]
+        if max(liste_distance)<=2*min(liste_distance):
+
+            if distance(liste_pos[0],liste_pos[-1])<30:
+                return True,(centre,max(liste_distance))
+
+    return False,(None,None)
+
+def r_ellipse(liste_pos):
+
+    liste_x=[i[0] for i in liste_pos]
+    liste_y=[i[1] for i in liste_pos]
+
+
+    for point in liste_pos:
+            if point[0]==max(liste_x):
+                a=point
+            if point[1]==min(liste_y):
+                b=point
+            if point[0]==min(liste_x):
+                c=point
+            if point[1]==max(liste_y):
+                d=point
+    centre=interdiagonalequadri(a,b,c,d)
+
+    if centre!='Erreur':
+
+        axe_x=droite(a,c)
+
+        d_a=distance(centre,a)
+        d_b=distance(centre,b)
+        if d_a<d_b:
+            d_a,d_b=d_b,d_a
+
+        d_c=math.sqrt(d_a**2-d_b**2)
+
+        foyers=[i for i in axe_x if abs(distance(centre,i)-d_c)<3]
+        try:
+            f1,f2=foyers[0],foyers[-1]
+        except IndexError:
+            f1,f2=(0,0),(0,0)
+
+        liste_d_somme=[distance(point,f1)+distance(point,f2) for point in liste_pos]
+
+        if max(liste_d_somme)<=2*min(liste_d_somme):
+
+            if distance(liste_pos[0],liste_pos[-1])<30:
+                return True,(centre,(d_a,d_b))
+
+    return False,(None,(None,None))
+
+
+
+
+
+def r_arc_cercle(liste_pos):
+
+    liste_x=[i[0] for i in liste_pos]
+    liste_y=[i[1] for i in liste_pos]
+
+    if liste_y==sorted(liste_y) or liste_y==reversed(sorted(liste_y)):
+
+        liste_pics,l_croissance=croissance(liste_x)
+        nb_pics=len(liste_pics)
+
+        if nb_pics==1:
+
+            if l_croissance==[True,False]:
+                return "droite"
+            elif l_croissance==[False,True]:
+                return "gauche"
+
+
+
+    return False
+
+
+def r_tp(liste_pos,rect_perso,liste_tp):
+
+    """ La liste retourne l'indice du rectangle de téléportation. Sinon, retourne -1
+    """
+
+    deb=liste_pos[0]
+    fin=liste_pos[-1]
+
+    if rect_perso.collidepoint(deb):
+        for i,rect in enumerate(liste_tp):
+            if rect.collidepoint(fin):
+                return i
+    elif rect_perso.collidepoint(fin):
+        for i,rect in enumerate(liste_tp):
+            if rect.collidepoint(deb):
+                return i
+
+
+    return -1
+
