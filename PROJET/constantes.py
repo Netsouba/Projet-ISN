d4a5e79e77fa62ff49dc52408b5579660e933f72
Netsouba <netsouba@gmail.com>
2017-05-06 18:29:36 +0200

    Update constantes.py

--- PROJET/constantes.py
+++ PROJET/constantes.py
@@ -7,22 +7,23 @@ from copy import deepcopy
 
 #----------------------------Constantes----------------------------------
 
-#Dimensions
-fenetre_x=960
-fenetre_y=540
-taille_fenetre=fenetre_x,fenetre_y
+#---------------------------Dimensions-----------------------------------
+fenetre_x=960                             #Largeur de la fenêtre
+fenetre_y=540                             #Hauteur de la fenêtre
+taille_fenetre=fenetre_x,fenetre_y        #Taille de la fenêtre
+                                          #Ces constantes seront utiles notamment pour centrer des images ou des textes
+  
+bloc_x=30                                 #Largeur d'un bloc  
+bloc_y=30                                 #Hauteur d'un bloc
+taille_bloc=bloc_x,bloc_y                 #Taille d'un bloc
+                                          #Ces constantes seront utiles pour le quadrillage de l'écran
+  
+nb_bloc_x=fenetre_x//bloc_x               #Nombre de blocs de gauche à droite
+nb_bloc_y=fenetre_y//bloc_y               #Nombre de blocs de haut en bas
 
-bloc_x=30
-bloc_y=30
-taille_bloc=bloc_x,bloc_y
 
-nb_bloc_x=fenetre_x//bloc_x
-nb_bloc_y=fenetre_y//bloc_y
-
-taille_perso=50,50
-
-#Couleurs
-BLANC=255,255,255
+#--------------------------------Couleurs------------------------------------
+BLANC=255,255,255                         #Des constantes sur le RGB des couleurs est utile pour raccourcir et simplifier le programme
 GRIS=127,127,127
 NOIR=0,0,0
 ROUGE=255,0,0
@@ -32,12 +33,14 @@ JAUNE=255,255,0
 CYAN=0,255,255
 MAGENTA=255,0,255
 
-#Events
-POP_BULLE=24
-ANIMER=25
-INVINCIBLE=26
-RALENTI=27
+#-----------------------------------Events-----------------------------------
+                                          #Des USERSEVENTS sont mis à disposition par pygame pour créer ses propres évenements
+POP_BULLE=24                              #L'évenement sera appelé quand une bulle doit éclater
+ANIMER=25                                 #L'évement est appelé régulierement pour animer les objets
+INVINCIBLE=26                             #L'évenement sera appelé quand l'invincibilité du personnage doit d'arreter
+RALENTI=27                                #L'évenement sera appelé quand le rallentissement des ennemis est fini
 
 
-#Divers
-g=0.3
+#------------------------------------Divers-----------------------------------
+g=0.3                                     #Valeur arbitraire de l'acceleration dûe à la gravitation
+                                          #Cela signifie que les objets accelerent de 0.3 pixels par frame vers le bas.
