df12dbe5fc2811ab5c7f8a89bee3120109a0ae6b
Netsouba <netsouba@gmail.com>
2017-05-07 15:56:44 +0200

    Update main.py

--- PROJET/main.py
+++ PROJET/main.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 import pygame
 from pygame.locals import *
 import math
@@ -20,21 +20,22 @@ pygame.display.set_caption("C'est mon jeu!!!")                  #Création du ti
 
 
 #---------------------------Creation des niveaux------------------------
-for i in range(19):                                             #On fait une boucle pour créer tous les niveaux
+for i in range(24):                                             #On fait une boucle pour créer tous les niveaux
     if i==1:pos=40,40                                           #La position de départ change selon le numero du niveau
     elif i==18:pos=40,110
+    elif i==19:pos=40,40
     else:pos=40,480
     Niveau(i,img_niveau,pos)                                    #On créer l'objet Niveau.
 
 if accueil()=="continuer":                                      #On lance l'écran d'accueil. On continue si il a choisi de continuer.
     continuer=True                                              #On fait une boucle infini qui exécute le menu et le jeu.
-    while continuer:                        
-        
+    while continuer:
+
         niveau_actuel=menu()                                    #On excécute le menu et on capture le numero choisi
 
-        if niveau_actuel!=None:                                 #On teste si l'utilisateur n'a pas quitté le jeu                  
-            musique("Sons/music_jeu.wav")                       #On lance la musique grâce à notre fonction musique                
-            while True:                                         #On fait une boucle infini qui éxecute le jeu                                
+        if niveau_actuel!=None:                                 #On teste si l'utilisateur n'a pas quitté le jeu
+            musique("Sons/music_jeu.wav")                       #On lance la musique grâce à notre fonction musique
+            while True:                                         #On fait une boucle infini qui éxecute le jeu
 
                 if niveau_actuel<len(Niveau.liste):             #Si le niveau à executer est cohérent par rapport à la liste des niveaux
                     etat=jeu(Niveau.liste[niveau_actuel])       #On exécute le jeu, et on capture dans etat quelque chose si le jeu est fini
@@ -45,16 +46,16 @@ if accueil()=="continuer":                                      #On lance l'écr
                     elif etat=="reset":                         #Si le joueur doit recommencer le niveau
                         pass                                    #On ne fait rien, donc la boucle recommence, avec le même niveau
                     elif etat=="fin":                           #Si le joueur quitte le jeu
-                        continuer=False                         #On sort de la boucle de la ligne 31  
-                        break                                   #On sort de la boucle de la ligne 37   
+                        continuer=False                         #On sort de la boucle de la ligne 31
+                        break                                   #On sort de la boucle de la ligne 37
 
                 else:                                           #Si le niveau à executer n'appartient plus à la liste des niveaux
-                    pygame.image.save(fenetre,"temp/save.png")  #On fait une capture de l'écran         
+                    pygame.image.save(fenetre,"temp/save.png")  #On fait une capture de l'écran
                     img_vctr=pygame.image.load("temp/save.png")
                     v=victoire(img_vctr)                        #Puis on exécute l'écran de victoire
                     if v=="fin":                                #Si l'utilisateur a quitté dans l'écran de victoire:
                         continuer=False                         #On sort de la boucle de la ligne 31
-                        break                                   #On sort de la boucle de la ligne 37                   
+                        break                                   #On sort de la boucle de la ligne 37
                     elif v=="menu":                             #Si l'utilisateur a choisi de retourner au menu:
                         son_victory.stop()                      #On stoppe l'effet sonore de la victoire
                         pygame.mixer.music.play()               #On rejoue la musique
@@ -67,4 +68,3 @@ if accueil()=="continuer":                                      #On lance l'écr
             continuer=False                                     #On quitte la boucle
 
 pygame.quit()                                                   #Une fois que l'utilisateur a quitté les deux boucles, on déinitialise pygame et le programme est fini
-
